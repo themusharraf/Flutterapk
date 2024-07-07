e27e35208c25899be794f9ba7d3d9903e41e83ce
msi <meibrohimov@gmail.com>
2024-07-08 02:21:54 +0500

    add test main.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,48 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:myapp/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Navigation test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MaterialApp());
+    await tester.pumpWidget(MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const HomePage(),
+        '/login': (context) => const LoginPage(),
+        '/Register': (context) => const RegisterPage(),
+      },
+    ));
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Verify that HomePage is displayed.
+    expect(find.text('Home Page welcome!'), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    // Tap the 'Login' button and trigger a frame.
+    await tester.tap(find.text('Login'));
+    await tester.pumpAndSettle();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify that LoginPage is displayed.
+    expect(find.text('Login Page'), findsOneWidget);
+
+    // Tap the 'Back' button and trigger a frame.
+    await tester.tap(find.text('Back'));
+    await tester.pumpAndSettle();
+
+    // Verify that HomePage is displayed again.
+    expect(find.text('Home Page welcome!'), findsOneWidget);
+
+    // Tap the 'Register' button and trigger a frame.
+    await tester.tap(find.text('Register'));
+    await tester.pumpAndSettle();
+
+    // Verify that RegisterPage is displayed.
+    expect(find.text('Register Page'), findsOneWidget);
+
+    // Tap the 'Back' button and trigger a frame.
+    await tester.tap(find.text('Back'));
+    await tester.pumpAndSettle();
+
+    // Verify that HomePage is displayed again.
+    expect(find.text('Home Page welcome!'), findsOneWidget);
   });
 }
